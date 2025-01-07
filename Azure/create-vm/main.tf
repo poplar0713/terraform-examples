d9a30f06bd9c45c0229f53415089f4ac27be7848
Jeongdong <duwjdehd0713@gmail.com>
2025-01-07 14:14:07 +0900

    Update main.tf

--- Azure/create-vm/main.tf
+++ Azure/create-vm/main.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "example" {
@@ -26,12 +21,24 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_network_interface" "example" {
+  name                = var.network_interface_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 resource "azurerm_linux_virtual_machine" "example" {
   name                  = var.vm_name
   resource_group_name   = azurerm_resource_group.example.name
   location              = azurerm_resource_group.example.location
   size                  = var.vm_size
-  network_interface_ids = [azurerm_virtual_network.example.id]
+  network_interface_ids = [azurerm_network_interface.example.id]
 
   admin_username = var.admin_username
   admin_password = var.admin_password
@@ -39,12 +46,13 @@ resource "azurerm_linux_virtual_machine" "example" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    disk_size_gb         = var.os_disk_size
   }
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
   }
 }

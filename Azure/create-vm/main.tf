d3553eae3df629965116b37c5d78a073199979aa
Jeongdong <duwjdehd0713@gmail.com>
2025-01-10 15:29:00 +0900

    Update main.tf

--- Azure/create-vm/main.tf
+++ Azure/create-vm/main.tf
@@ -53,7 +53,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "19.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }

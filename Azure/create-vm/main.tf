eb1390494818dd3bdc8ddecad438a1e12c7393b0
Jeongdong <duwjdehd0713@gmail.com>
2025-01-10 16:22:07 +0900

    Update main.tf

--- Azure/create-vm/main.tf
+++ Azure/create-vm/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
-  location = var.location
+  location = var.resource_group_location
 }
 
 resource "azurerm_virtual_network" "example" {
